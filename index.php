4a94379be093b3d9c11636001735a3447837618c
Melissa Cabral <melissacabral@gmail.com>
2015-11-25 19:44:37 -0800

    super basic rating script

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php require('config.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rate this thing</title>
+	<link rel="stylesheet" type="text/css" href="rating.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+</head>
+<body>
+<h1>Rate this thing!</h1>
+
+<form action="#" method="post">
+	<span class="star-rating">
+	  <input type="radio" name="rating" value="1" data-id="1"><i></i>
+	  <input type="radio" name="rating" value="2" data-id="1"><i></i>
+	  <input type="radio" name="rating" value="3" data-id="1"><i></i>
+	  <input type="radio" name="rating" value="4" data-id="1"><i></i>
+	  <input type="radio" name="rating" value="5" data-id="1"><i></i>
+	</span>
+
+
+</form>
+<div id="display-area"></div>
+
+<script type="text/javascript">
+        $(":radio").change(function() { 
+            //get the value of the category they clicked
+            var rating = this.value;      
+            var thing_id = $(this).data("id");        
+            //create an ajax request to display.php
+            $.ajax({   
+                type: "GET",
+                url: "rate-parse.php",  
+                data: { 'rating': rating, 'thing_id' : thing_id },   
+                dataType: "html",   //expect html to be returned
+              success: function(response){
+                $("#display-area").html(response);
+                }
+            });
+        });
+         //do stuff during and after ajax is loading (like visual feedback)
+        $(document).on({
+            ajaxStart: function() { $("#display-area").addClass("loading");    },
+            ajaxStop: function() { $("#display-area").removeClass("loading"); } 
+        });
+    </script>
+</body>
+</html>
---
+++ rate-parse.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * DISPLAY OUTPUT
+ * This file gets loaded by the ajax request
+ * It is identical whether using jquery or pure ajax
+ * note that it has no doctype and is not intended as a standalone file. 
+ */
+require('config.php');
+
+//data coming from jquery .ajax() call
+$id = $_REQUEST['thing_id'];
+$rating = $_REQUEST['rating'];
+
+//add the rating to the db
+$query = "INSERT INTO ratings 
+		(thing_id, rating)
+		VALUES 
+		($id, $rating)";
+//run it
+$result = $db->query($query);
+
+//check
+if($db->affected_rows == 1){
+	echo 'Thanks for rating '. $rating .' stars';
+	
+	//calculate the new average rating
+	$query = "SELECT AVG(rating) AS average FROM ratings WHERE thing_id = $id";
+	$result = $db->query($query);
+	$row = $result->fetch_assoc();
+	echo '<br>The average rating is '. round($row['average']);
+	
+}else{
+	echo 'Sorry, the rating did not work';
+}
